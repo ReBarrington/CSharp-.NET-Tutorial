be5396ddf8d0f695c68c3378cbf28788c15bd455
unknown <reagan@barringtonfamily.net>
2021-03-25 10:33:12 -0400

    grade statistics

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Book
+    {
+        public Book(string name) 
+        // constructor
+        {
+            grades = new List<double>();
+            this.name = name;
+        }
+
+        public void AddGrade(double grade)
+        // void because it will not return anything
+        {
+            grades.Add(grade);
+        }
+
+        public void ShowStatistics() {
+            var average = 0.0;
+            var highestGrade = double.MinValue;
+            var lowestGrade = double.MaxValue;
+
+            foreach(var number in grades)
+            {   
+                highestGrade = Math.Max(number, highestGrade);
+                lowestGrade = Math.Min(number, lowestGrade);
+                average += number;
+            }
+            average /= grades.Count;
+            // N1 is number with one decimal
+            Console.WriteLine($"The lowest grade is {lowestGrade}");
+            Console.WriteLine($"The highest grade is {highestGrade}");
+            Console.WriteLine($"The average grade is {average:N1}");
+        }
+
+        // fields
+        private List<double> grades;
+        private string name;
+    }
+}
---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        // Main is a method with a parameter- type and name
+        // static only available for the type name vs public
+        static void Main(string[] args)
+        {
+            var book = new Book("Scott's Grade Book");
+            book.AddGrade(91);
+            book.AddGrade(90.5);
+            book.AddGrade(77.5);
+            book.ShowStatistics();
+        }
+    }
+}
